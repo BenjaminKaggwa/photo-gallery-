fcfa804ebd97b86fdb179f434c7d060bd895ade7
Benja <la@Benjas-MacBook-Pro.local>
2025-11-18 23:52:25 +0800

    Add photo gallery with random images and load more button

---
+++ Desktop/Pinterest clone/index.js
@@ -0,0 +1,11 @@
+const button=document.querySelector("button")
+button.addEventListener("click",() =>{
+    const random=Math.floor(Math.random()*2000)
+        
+        img.src="https://picsum.photos/300?random"
+
+    // make a random number between 1 and 2k, only whole numbers
+    // add it top the api 
+    // add to the image tage through src 
+})
+
